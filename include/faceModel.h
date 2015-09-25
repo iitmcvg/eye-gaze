2b3459186f086fe39beaeab03ca64345c51a2051
mrupeshkumar <dmrupeshkumar@gmail.com>
2015-09-25 16:28:49 +0530

    working on faceModel.cpp

--- include/faceModel.h
+++ include/faceModel.h
@@ -1,7 +1,7 @@
 #ifndef FACE_MODEL_H
 #define FACE_MODEL_H
 
-struct Face {
+class Face {
 
 	static const int MODE_LEFT = 0;
 	static const int MODE_RIGHT = 1;
@@ -14,14 +14,23 @@ struct Face {
 	static const int INDEX_MOUTH_OUTER = 5;
 	static const int INDEX_MOUTH_INNER = 6;
 
+
 	full_object_detection faceShape;
+	static cv::Point origin = cv::Point(0,0);
 
 	double yaw, pitch, sigma, symm_x, theta, tau;
 	vector<double> normal, gaze;
 
+Public:
+
     void assign(full_object_detection shape);
     cv::Point getPupil(int mode);
-    cv::Point getFeatureDescriptors(int index);
+    std::vector<cv::Point> getFeatureDescriptors(int index);
+    std::vector<double> getGaze();
+    std::vector<double> getNormal();
+    void setOrigin(cv::Point origin);
+    void setOrigin(int mode);
+
 };
 
 #endif
--- src/faceModel.cpp
+++ src/faceModel.cpp
@@ -15,9 +15,69 @@
 #include "constants.h"
 #include "faceDetection.h"
 
-using namespace std;
-using namespace dlib;
+// using namespace std;
+// using namespace dlib;
 
 void faceModel::assign(full_object_detection shape) {
 	faceShape = shape;
+}
+
+cv::Point faceModel::getPupil(int mode) {
+
+
+}
+
+std::vector<cv::Point> faceModel::getFeatureDescriptors(int index) {
+
+	if (index == INDEX_LEFT_EYE) {
+
+		cv::Point points[] = {cv::Point(faceShape.part(36).x(), faceShape.part(36).y()), cv::Point(faceShape.part(37).x(), faceShape.part(37).y()), 
+			cv::Point(faceShape.part(38).x(), faceShape.part(38).y()), cv::Point(faceShape.part(39).x(), faceShape.part(39).y()), 
+			cv::Point(faceShape.part(40).x(), faceShape.part(40).y()), cv::Point(faceShape.part(41).x(), faceShape.part(41).y())};
+		
+		std::vector<cv::Point> leftEyePoints;
+		for (int i=0; i<6; i++){
+			leftEyePoints.push_back(points[i]);
+		}
+	return leftEyePoints;
+	}
+
+	else if (index == INDEX_RIGHT_EYE) {
+
+		cv::Point points[] = {cv::Point(faceShape.part(42).x(), faceShape.part(42).y()), cv::Point(faceShape.part(43).x(), faceShape.part(43).y()), 
+			cv::Point(faceShape.part(44).x(), faceShape.part(44).y()), cv::Point(faceShape.part(45).x(), faceShape.part(45).y()), 
+			cv::Point(faceShape.part(46).x(), faceShape.part(46).y()), cv::Point(faceShape.part(47).x(), faceShape.part(47).y())};
+		
+		std::vector<cv::Point> rightEyePoints;
+		for (int i=0; i<6; i++){
+			rightEyePoints.push_back(points[i]);
+		}
+	return rightEyePoints;
+	}
+
+	else if (index == INDEX_LEFT_EYE_BROW) {
+
+		cv::Point points[] = {cv::Point(faceShape.part(17).x(), faceShape.part(17).y()), cv::Point(faceShape.part(18).x(), faceShape.part(18).y()), 
+			cv::Point(faceShape.part(19).x(), faceShape.part(19).y()), cv::Point(faceShape.part(20).x(), faceShape.part(20).y()), 
+			cv::Point(faceShape.part(21).x(), faceShape.part(21).y())};
+		
+		std::vector<cv::Point> leftEyeBrowPoints;
+		for (int i=0; i<5; i++){
+			leftEyeBrowPoints.push_back(points[i]);
+		}
+	return leftEyeBrowPoints;
+	}
+
+	else if (index == INDEX_RIGHT_EYE_BROW) {
+
+		cv::Point points[] = {cv::Point(faceShape.part(22).x(), faceShape.part(22).y()), cv::Point(faceShape.part(23).x(), faceShape.part(23).y()), 
+			cv::Point(faceShape.part(24).x(), faceShape.part(24).y()), cv::Point(faceShape.part(25).x(), faceShape.part(25).y()), 
+			cv::Point(faceShape.part(26).x(), faceShape.part(26).y())};
+		
+		std::vector<cv::Point> rightEyeBrowPoints;
+		for (int i=0; i<5; i++){
+			rightEyeBrowPoints.push_back(points[i]);
+		}
+	return rightEyeBrowPoints;
+	}
 }
